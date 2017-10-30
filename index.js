3ced2071eaaccb2092de5272001118d9554b9895
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-30 10:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndvalue(object, key, value) {
+  object.[key] = value;
+  return object;
+}
+
